84c357ce637a90b8aaeff3044e396bd9ce0baaa2
yizhixiaokong <wdhxx20121@126.com>
2020-01-11 16:58:30 +0800

    easy web test

---
+++ learn-7/go-7.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+)
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()       //解析参数
+	fmt.Println(r.Form) //输出到服务端的打印信息
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
+
+	fmt.Fprintf(w, "Hello World! %s", time.Now()) //输出到客户端的信息
+}
+
+func main() {
+	http.HandleFunc("/", greet)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
---
+++ learn-7/go.mod
@@ -0,0 +1,3 @@
+module go-7
+
+go 1.13
